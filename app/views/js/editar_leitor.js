d5586c4fd999a269fdc54ebce2c3f059de25e53f
JoaoPedro2001 <147657992+JoaoPedro2001@users.noreply.github.com>
2026-05-17 14:31:08 -0300

    Operações CRUD para leitores via Frontend
    Criação das telas e códigos responsáveis pela realização das operações de CRUD via Frontend para tabela de leitores.

---
+++ app/views/js/editar_leitor.js
@@ -0,0 +1,92 @@
+const parametros = new URLSearchParams(
+    window.location.search
+)
+
+const leitorId = parametros.get("id")
+
+async function carregarLeitor() {
+
+    const resposta = await fetch(
+        `http://127.0.0.1:5000/leitores/${leitorId}`
+    )
+
+    const leitor = await resposta.json()
+
+    document.getElementById(
+        "nome"
+    ).value = leitor.nome
+
+    document.getElementById(
+        "email"
+    ).value = leitor.email
+
+    document.getElementById(
+        "telefone"
+    ).value = leitor.telefone
+
+}
+
+carregarLeitor()
+
+const formulario = document.getElementById(
+    "form-leitor"
+)
+
+formulario.addEventListener(
+    "submit",
+    async function(event) {
+
+        event.preventDefault()
+
+        const leitor = {
+
+            nome: document.getElementById(
+                "nome"
+            ).value,
+
+            email: document.getElementById(
+                "email"
+            ).value,
+
+            telefone: document.getElementById(
+                "telefone"
+            ).value,
+
+        }
+
+        const resposta = await fetch(
+            `http://127.0.0.1:5000/leitores/${leitorId}`,
+            {
+
+                method: "PUT",
+
+                headers: {
+                    "Content-Type": "application/json"
+                },
+
+                body: JSON.stringify(leitor)
+
+            }
+        )
+
+        if (resposta.ok) {
+
+            alert(
+                "Leitor atualizado com sucesso!"
+            )
+
+            window.location.href =
+                "visualizacao_leitores.html"
+
+        }
+
+        else {
+
+            alert(
+                "Erro ao atualizar leitor."
+            )
+
+        }
+
+    }
+)
